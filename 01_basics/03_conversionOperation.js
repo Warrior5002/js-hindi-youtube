13175d5cb9a9939745a6c217629c37ca2a15bea4
Akash Bisoyi <abisoyi472@gmail.com>
2025-09-01 06:28:56 +0000

    done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,7 +1,13 @@
-let score = "33"
+let score = "33ABC"
+
+console.log(typeof score);
 
 console.log(typeof(score));
 
-console.log(typeof(score));
+let valueInNumber = Number (score);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
 
 
