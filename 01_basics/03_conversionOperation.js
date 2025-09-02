3fdfe244c684d93db6a05fd2dd94d61c1d3aeee5
Akash Bisoyi <abisoyi472@gmail.com>
2025-09-02 06:09:44 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -9,5 +9,25 @@ let valueInNumber = Number (score);
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
 
+//"33" => 33
+//"33abc" => Nan
+//true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+// 1=> true; 0=> false
+// ""=> false
+// "akash" => true
+
+let someNumber = 33
+
+let StringNumber = String (someNumber)
+console.log(StringNumber)
+console.log(typeof stringNumber);
+
 
 
